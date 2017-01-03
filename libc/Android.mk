9f731c445edccdf194356953909bc3590cb75df0
Alex Naidis <alex.naidis@linux.com>
2017-01-03 01:41:18 +0100

    libc: Improve CAF libc wrapper flag config
    The CAF changes 0ca32a9 and 153ed5a
    were made following a wrong logic,
    thinking that CFLAGS are not getting
    applied to CPP files.
    In reality CFLAGS get applied to C
    and CPP files.
    
    Applying "USE_WRAPPER" to CPP files
    only is potentially unsafe, since
    the header "unistd.h" picks it up
    and its "write" method definition
    can be picked up by C files too.
    
    This patch converts "USE_WRAPPER"
    to be applied on C and CPP files
    in the needed cases and removes
    redunant definitions which result
    from this change.
    
    Change-Id: I0f268b49944ee2b3c4bcd946b8fa0a1f4786f224
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- libc/Android.mk
+++ libc/Android.mk
@@ -1028,7 +1028,7 @@ LOCAL_CPPFLAGS := $(libc_common_cppflags) -Wold-style-cast
 
 
 ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
-LOCAL_CPPFLAGS += -DUSE_WRAPPER
+LOCAL_CFLAGS += -DUSE_WRAPPER
 endif
 
 LOCAL_C_INCLUDES := $(libc_common_c_includes) bionic/libstdc++/include
@@ -1064,10 +1064,6 @@ endif
 
 LOCAL_CONLYFLAGS := $(libc_common_conlyflags)
 LOCAL_CPPFLAGS := $(libc_common_cppflags) -Wold-style-cast \
-
-ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
-LOCAL_CPPFLAGS += -DUSE_WRAPPER
-endif
 
 LOCAL_C_INCLUDES := $(libc_common_c_includes) bionic/libstdc++/include
 LOCAL_MODULE := libc_bionic_ndk
@@ -1256,6 +1252,11 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(libc_common_src_files)
 LOCAL_CFLAGS := $(libc_common_cflags)
+
+ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
+LOCAL_CFLAGS += -DUSE_WRAPPER
+endif
+
 LOCAL_CONLYFLAGS := $(libc_common_conlyflags)
 LOCAL_CPPFLAGS := $(libc_common_cppflags)
 LOCAL_C_INCLUDES := $(libc_common_c_includes)
@@ -1403,7 +1404,7 @@ LOCAL_SRC_FILES := \
 
 ifeq ($(BOARD_USES_LIBC_WRAPPER),true)
     LOCAL_SRC_FILES += codeaurora/PropClient.cpp
-    LOCAL_CPPFLAGS += -DUSE_WRAPPER
+    LOCAL_CFLAGS += -DUSE_WRAPPER
 endif
 
 LOCAL_MODULE := libc
