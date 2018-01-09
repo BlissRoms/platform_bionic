Unlegacy-Android/android_bionic

[1/2] linker: Make platform text relocations denial optional
 * Use the TARGET_NEEDS_PLATFORM_TEXTRELS := true
    configuration to allow a device to use legacy proprietary
    libraries like camera on non-user build variants

 * Partial revert "Remove textrels support for platform libs"
    commit 8068786ae67835291521e52f39c695e40f3ad20d.

 * Forward-port to AOSP 8.0, use __ANDROID_API_M__
    and adapt to Android.bp build rules

Change-Id: I994ab1a600a0b237b496ceebe2dd54febc28a6bd
Signed-off-by: Adrian DC <radian.dc@gmail.com>

linker/linker.cpp
@@ -3279,7 +3279,12 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
 #if !defined(__LP64__)
   if (has_text_relocations) {
     // Fail if app is targeting M or above.
+#if defined(ALLOW_PLATFORM_TEXTRELS)
+    if (get_application_target_sdk_version() != __ANDROID_API__
+        && get_application_target_sdk_version() >= __ANDROID_API_M__) {
+#else
     if (get_application_target_sdk_version() >= __ANDROID_API_M__) {
+#endif
       DL_ERR_AND_LOG("\"%s\" has text relocations", get_realpath());
       return false;
     }

BlissRoms/platform_bionic

linker/linker.cpp
@@ -3407,7 +3407,12 @@ bool soinfo::link_image(const soinfo_list_t& global_group, const soinfo_list_t& 
 #if !defined(__LP64__)
   if (has_text_relocations) {
     // Fail if app is targeting M or above.
+#if defined(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)
+    if (get_application_target_sdk_version() != __ANDROID_API__
+        && get_application_target_sdk_version() >= __ANDROID_API_M__) {
+#else
     if (get_application_target_sdk_version() >= __ANDROID_API_M__) {
+#endif
       DL_ERR_AND_LOG("\"%s\" has text relocations (https://android.googlesource.com/platform/"
                      "bionic/+/master/android-changes-for-ndk-developers.md#Text-Relocations-"
                      "Enforced-for-API-level-23)", get_realpath());